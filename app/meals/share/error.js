e2ca2237e2ab50597f537c15f303c3c82dd956fd
rcarlosjorge <carlosepjorge@gmail.com>
2024-04-28 12:52:17 -0400

    Version 1 completed

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An Error Occurred!</h1>
+      <p>Failed to create meal. Please try again later.</p>
+    </main>
+  );
+}
